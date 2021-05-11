df640ca6cfbbd555a528fa38bb326e362307c61d
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-05-11 13:41:26 -0300

    Primeiro contato com testes unitários

---
+++ junit/src/test/java/org/generation/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.junit;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
